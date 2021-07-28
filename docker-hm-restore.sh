cbbbbf2db70d7efa88b8709e8adf241fc47622ab
Adipsys <29979292+Adipsys@users.noreply.github.com>
2021-07-28 09:55:09 +0200

    Update docker-hm-restore.sh

--- docker-hm-restore.sh
+++ docker-hm-restore.sh
@@ -29,52 +29,55 @@ done
 
 DATE=`date +%Y-%m-%d-%H-%M-%S`
 
-
-if [ ! -f $DATABASEFILE ] ; then
-        echo "Missing mandatory $DATABASEFILE !"
-        echo $USAGE
-	exit 1
-fi
-
-if [ ! -f $VOLUMEDATAFILE ] ; then
-        echo "Missing mandatory VOLUMEDATAFILE !"
-        echo $USAGE
-	exit 1
-fi
-
-if [ -z $TARGETVOL ] ; then
-        echo "Missing mandatory $TARGETVOL parameter !"
-        echo $USAGE
-	exit 1
-fi
-
-if [ -z $MAINCONTAINER ] ; then
-        echo "Missing mandatory MAINCONTAINER parameter !"
-        echo $USAGE
-	exit 1
-fi
-
-if [ -z $DBPASSWORD ]; then
-        echo "Missing mandatory DBPASSWORD parameter !"
-        exit 1
-fi
-
 restore_database()
 {
-	CONTAINERFULLNAME=$(docker ps --format "{{.Names}}" | grep "$MAINCONTAINER")
+    if [ -z $MAINCONTAINER ] ; then
+        echo "Missing mandatory MAINCONTAINER parameter !"
+        echo $USAGE
+        exit 1
+    else
+        CONTAINERFULLNAME=$(docker ps --format "{{.Names}}" | grep "$MAINCONTAINER")
+    fi
+    
+    if [ -z $DBPASSWORD ]; then
+        echo "Missing mandatory DBPASSWORD parameter !"
+        exit 1
+    fi
+    
+    echo "WARNING : TARGET DATABASE on $MAINCONTAINER WILL BE OVERWRITTEN !!!!! (CTRL+C TO CANCEL)"
+    read
+    
 	echo -e "- Restoring $DATABASEFILE to $CONTAINERFULLNAME\n"
 	gunzip < $DATABASEFILE | docker exec -i $CONTAINERFULLNAME mysql -h hm-db -u root -p$DBPASSWORD
 }
 
 restore_volume()
 {
+    if [ -z "$TARGETVOL" ] ; then
+        echo "Missing mandatory TARGETVOL parameter !"
+        echo $USAGE
+        exit 1
+    fi
+    echo "WARNING : TARGET VOLUME $TARGETVOL WILL BE OVERWRITTEN !!!!! (CTRL+C TO CANCEL)"
+    read
+    
 	echo "- Restoring $VOLUMEDATAFILE to $TARGETVOL"
 	docker run --rm -v $TARGETVOL:/var/www -v $VOLUMEDATAFILE:/backups/volume.tar.gz debian:jessie-slim bash -c "cd /var/www && tar xzf /backups/volume.tar.gz ."
 	echo -e "\n Please restart the main container"
 }
 
-echo "WARNING : TARGET DATABASE on $MAINCONTAINER and VOLUME $TARGETVOL WILL BE OVERWRITTEN !!!!! (CTRL+C TO CANCEL)"
-read
-
-restore_database
-restore_volume
+    if [ -n "$DATABASEFILE" ] || [ -n "$VOLUMEDATAFILE" ] ; then
+                if [ -f "$DATABASEFILE" ]; then
+                restore_database
+                else
+                    echo "$DATABASEFILE not found !"
+                fi
+                
+                if [ -f "$VOLUMEDATAFILE" ]; then
+                restore_volume
+                else
+                    echo "$VOLUMEDATAFILE not found !"
+                fi
+    else
+        echo -e "No DATABASEFILE or VOLUMEDATAFILE\n$USAGE"
+    fi
